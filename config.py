6143236decc102d6602bba9d2c34a354eec8daa4
yongliao666 <yongliao@hotmail.com>
2025-02-04 16:46:58 +0800

    添加阿里云api_key

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # DeepSeek API密钥
 API_KEY = "sk-200ef1d9e2f84952aad41a1e43c2c9d0"
 #google API_KEY='AIzaSyAQUVx73KF0qdZqOWav1E6br2XUGKsk2Ns'
+#aliyun ap_key=	'sk-7ba685cbecf44c059c9a9e1d17ef37e6'
+#aliyun baseurl ='https://dashscope.aliyuncs.com/compatible-mode/v1'
